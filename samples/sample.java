39941b0349ca1285c36fe487c26022fc397f0fda
Ypthan0206 <alanhenao0206@gmail.com>
2025-03-06 23:11:02 -0500

    Update sample.java

--- samples/sample.java
+++ samples/sample.java
@@ -1,5 +1,20 @@
-public class sample {
+public class Main {
+    public static boolean esPrimo(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++)
+            if (n % i == 0) return false;
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hola, mundo");
+        int contador = 0;
+        System.out.println("Números primos entre 0 y 300:");
+        for (int i = 0; i <= 300; i++) {
+            if (esPrimo(i)) {
+                System.out.print(i + " ");
+                contador++;
+            }
+        }
+        System.out.println("\nCantidad total: " + contador);
     }
 }
