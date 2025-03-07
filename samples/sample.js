e11a73d2955bb13567a7fe1862a5d662e24916fa
Ypthan0206 <alanhenao0206@gmail.com>
2025-03-06 23:11:18 -0500

    Update sample.js

--- samples/sample.js
+++ samples/sample.js
@@ -1,1 +1,21 @@
-console.log("Hola, mundo");
+function esPrimo(n) {
+    if (n < 2) return false;
+    for (let i = 2; i * i <= n; i++)
+        if (n % i === 0) return false;
+    return true;
+}
+
+let contador = 0;
+let primos = [];
+
+for (let i = 0; i <= 300; i++) {
+    if (esPrimo(i)) {
+        primos.push(i);
+        contador++;
+    }
+}
+
+console.log("Números primos entre 0 y 300:");
+console.log(primos.join(" "));
+console.log("Cantidad total:", contador);
+
