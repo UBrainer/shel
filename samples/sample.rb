c4f531abfe74ec658be05e5e4112d582327b4afd
Ypthan0206 <alanhenao0206@gmail.com>
2025-03-06 23:11:46 -0500

    Update sample.rb

--- samples/sample.rb
+++ samples/sample.rb
@@ -1,1 +1,12 @@
-puts "Hola, mundo"
+def es_primo(n)
+  return false if n < 2
+  (2..Math.sqrt(n)).each { |i| return false if n % i == 0 }
+  true
+end
+
+primos = (0..300).select { |i| es_primo(i) }
+contador = primos.length
+
+puts "Números primos entre 0 y 300:"
+puts primos.join(" ")
+puts "Cantidad total: #{contador}"
