2c601091c6153eb95fa1e570db70306a08bfa178
Ypthan0206 <alanhenao0206@gmail.com>
2025-03-06 23:10:44 -0500

    Update sample.cpp

--- samples/sample.cpp
+++ samples/sample.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
-using namespace std;
+
+bool esPrimo(int n) {
+    if (n < 2) return false;
+    for (int i = 2; i * i <= n; ++i)
+        if (n % i == 0) return false;
+    return true;
+}
 
 int main() {
-    cout << "Hola, mundo" << endl;
+    int contador = 0;
+    std::cout << "Números primos entre 0 y 300:\n";
+    for (int i = 0; i <= 300; ++i) {
+        if (esPrimo(i)) {
+            std::cout << i << " ";
+            contador++;
+        }
+    }
+    std::cout << "\nCantidad total: " << contador << std::endl;
     return 0;
 }
